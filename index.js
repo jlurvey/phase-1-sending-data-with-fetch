6053bb6e501cf456743d2cd648d291ead806759b
Joseph Lurvey <joeylurvey@gmail.com>
2023-05-25 21:27:47 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+submitData = (userName, userEmail) => {
+    const formData = {
+        name: userName,
+        email: userEmail,
+    };
+    
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    };
+    
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(response => response.json())
+    .then(function (object) {
+        document.body.append(object.id);       
+    })
+    .catch(function (error) {
+        document.body.append(error.message)
+    });
+}
